181b1202b5ea627056e8839e19f0f7e35ea2088b
AlenEvtikhov <50318546+AlenEvtikhov@users.noreply.github.com>
2022-05-16 15:25:24 +0300

    First diplom commit

---
+++ public/homePage.js
@@ -0,0 +1,97 @@
+'use strict';
+
+//Выход
+const logout = new LogoutButton();
+logout.action = exit => ApiConnector.logout(response => {
+	if (response.success) {
+		location.reload();
+		return;
+	}
+});
+
+//Инфо о пользователе
+ApiConnector.current(current => {
+	if (current.success) {
+		ProfileWidget.showProfile(current.data);
+		return;
+	}
+});
+
+//Инфо о курсах валют
+const rates = new RatesBoard();
+
+function ratesUpdate() {
+	ApiConnector.getStocks(response => {
+		if (response.success) {
+			rates.clearTable();
+			rates.fillTable(response.data);
+			return;
+		}
+	});
+}
+ratesUpdate();
+setInterval(ratesUpdate, 60000);
+
+//Операции с деньгами
+
+//Пополнение баланса
+
+const money = new MoneyManager();
+money.addMoneyCallback = credit => ApiConnector.addMoney(credit, response => {
+	if (response.success) {
+		ProfileWidget.showProfile(response.data);
+		return money.setMessage(true, 'Вы успешно пополнили счета на' + credit.currency + credit.amount);;
+	}
+	return money.setMessage(false, 'Ошибка: ' + response.error);
+});
+
+//Конвертация
+
+money.conversionMoneyCallback = exchange => ApiConnector.convertMoney(exchange, response => {
+	if (response.success) {
+		ProfileWidget.showProfile(response.data);
+		return money.setMessage(true, 'Конвертация успешно выполнена ' + exchange.fromCurrency + exchange.fromAmount);
+	}
+	return money.setMessage(false, 'Ошибка: ' + response.error);
+});
+
+//Перевод
+
+money.sendMoneyCallback = debit => ApiConnector.transferMoney(debit, response => {
+	if (response.success) {
+		ProfileWidget.showProfile(response.data);
+		return money.setMessage(true, 'Вы успешно выполнили перевод ' + debit.currency + debit.amount + ' получателю ' + debit.to);
+	}
+	return money.setMessage(false, 'Ошибка: ' + response.error);
+});
+
+//Добавление пользователя
+const favorite = new FavoritesWidget();
+ApiConnector.getFavorites(response => {
+	if (response.success) {
+		favorite.clearTable();
+		favorite.fillTable(response.data);
+		money.updateUsersList(response.data);
+		return;
+	}
+});
+favorite.addUserCallback = addUser => ApiConnector.addUserToFavorites(addUser, response => {
+	if (response.success) {
+		favorite.clearTable();
+		favorite.fillTable(response.data);
+		money.updateUsersList(response.data);
+		return money.setMessage(true, 'Добавлен новый пользователь #' + addUser.id + ': ' + addUser.name);
+	}
+	return money.setMessage(false, 'Ошибка: ' + response.error);
+});
+
+//Удаление пользователя
+favorite.removeUserCallback = deletedUser => ApiConnector.removeUserFromFavorites(deletedUser, response => {
+	if (response.success) {
+		favorite.clearTable();
+		favorite.fillTable(response.data);
+		money.updateUsersList(response.data);
+		return money.setMessage(true, 'Пользователь ' + deletedUser + ' удален');
+	}
+	return money.setMessage(false, 'Ошибка: ' + response.error);
+});
---
+++ public/loginPage.js
@@ -0,0 +1,18 @@
+'use strict'
+//Авторизация
+const userForm = new UserForm();
+userForm.loginFormCallback = data => ApiConnector.login(data, response => {
+	if (!response.success) {
+		return userForm.setLoginErrorMessage(response.error);
+	}
+	location.reload();
+	return;
+});
+//Регистрация
+userForm.registerFormCallback = newData => ApiConnector.register(newData, response => {
+	if (!response.success) {
+		return userForm.setRegisterErrorMessage(response.error);
+	}
+	location.reload();
+	return;
+});
